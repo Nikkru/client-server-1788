64ef901bc0eb5f6e48dd3985440e682c531a2f10
NikNik <nkru@narod.ru>
2021-12-11 19:22:13 +0300

    client_VK auth, get token

--- client-server-1788/VKServise/AuthorizationVKViewController.swift
+++ client-server-1788/VKServise/AuthorizationVKViewController.swift
@@ -11,11 +11,11 @@ import WebKit
 class AuthorizationVKViewController: UIViewController {
 
     @IBOutlet weak var webview: WKWebView!
-//    {
-//        didSet{
-//            webview.navigationDelegate = self
-//        }
-//    }
+    {
+        didSet{
+            webview.navigationDelegate = self
+        }
+    }
     
     private let appId = "8023112"
         
@@ -48,7 +48,35 @@ class AuthorizationVKViewController: UIViewController {
 
 }
 
-//extension AuthorizationVKViewController: WKNavigationDelegate {
-//
-//
-//}
+extension AuthorizationVKViewController: WKNavigationDelegate {
+
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationResponse: WKNavigationResponse,
+                 decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard let url = navigationResponse.response.url,
+              url.path == "/blank.html",
+              let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        let token = params["access_token"]
+        
+        print("token = \(token)")
+        
+        
+        decisionHandler(.cancel)
+    }
+}
