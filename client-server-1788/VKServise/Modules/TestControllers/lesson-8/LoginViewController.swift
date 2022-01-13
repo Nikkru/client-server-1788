77ccdb61dc7577332ba402c3127d3df78ecb681e
NikNik <nkru@narod.ru>
2022-01-13 23:36:15 +0300

    Authentication

--- client-server-1788/VKServise/Modules/TestControllers/lesson-8/LoginViewController.swift
+++ client-server-1788/VKServise/Modules/TestControllers/lesson-8/LoginViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailTextFIeld: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    private let authServise = Auth.auth()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +22,41 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func signInAction(_ sender: Any) {
+        
+        guard let email = emailTextFIeld.text,
+              emailTextFIeld.hasText,
+              let password = passwordTextField.text,
+              passwordTextField.hasText
+        else {
+            showAlert(title: "Ошибка на клиенте", text: "Не ввели логин или пароль")
+            return
+        }
+        
+        authServise.signIn(withEmail: email, password: password) { authResult, error in
+            
+            if let error = error {
+                self.showAlert(title: "Ошибка на сервере", text: error.localizedDescription)
+                return
+            }
+            
+            self.showHomeViewController()
+        }
     }
     @IBAction func signUpAction(_ sender: Any) {
     }
 
+    private func showHomeViewController() {
+        guard let vc = storyboard?.instantiateViewController(identifier: "HomeViewController") else { return }
+        guard let window = self.view.window else { return }
+        window.rootViewController = vc
+    }
+    
+    private func showAlert(title: String?, text: String?) {
+        let alert = UIAlertController(title: title,
+                                      message: text,
+                                      preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
