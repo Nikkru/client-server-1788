a5bdcbdaff8748ba9f01c157ea8c31fcb2344a76
NikNik <nkru@narod.ru>
2022-01-13 22:36:13 +0300

    lesson-8, Firebase

---
+++ client-server-1788/VKServise/Modules/TestControllers/lesson-8/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  client-server-1788
+//
+//  Created by Nikolai Krusser on 13.01.2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func signOutAction(_ sender: Any) {
+    }
+    @IBAction func addCityAction(_ sender: Any) {
+    }
+    
+}
---
+++ client-server-1788/VKServise/Modules/TestControllers/lesson-8/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  client-server-1788
+//
+//  Created by Nikolai Krusser on 13.01.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextFIeld: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func signInAction(_ sender: Any) {
+    }
+    @IBAction func signUpAction(_ sender: Any) {
+    }
+
+}
